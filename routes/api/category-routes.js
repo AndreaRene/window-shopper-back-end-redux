ce1edbcdeb91a9de3c595613a990a348bd975736
Andrea <andrea@interhact.net>
2022-11-03 19:01:24 -0500

    delete category route completed

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -55,8 +55,24 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+// delete a category
+router.delete('/:id', async (req, res) => {
+  try {
+    const category = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!category) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.json(category);
+  } catch (err) {
+    res.status(500).json(er.message);
+  }
 });
 
 module.exports = router;
